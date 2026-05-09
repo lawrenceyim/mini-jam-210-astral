449ed642bd421c5440a9887066741fdd282f03f9
lawrenceyim <lawrence.yim.dev@gmail.com>
2026-05-09 14:03:56 -0700

    Added SFX for scanning.

--- Scripts/TelescopeView.cs
+++ Scripts/TelescopeView.cs
@@ -2,138 +2,167 @@ using System;
 using Godot;
 
 public partial class TelescopeView : Node2D {
-	[Export]
-	private Camera2D _camera;
+    [Export]
+    private Camera2D _camera;
 
-	[Export]
-	private Sprite2D[] _stars;
+    [Export]
+    private Sprite2D[] _stars;
 
-	[Export]
-	private Vector2[] _constellationPositions;
+    [Export]
+    private Vector2[] _constellationPositions;
 
-	private bool[] _constellationFound;
+    [Export]
+    private AudioStreamPlayer _sfxPlayer;
 
-	private float _starsMaxScale = 1.5f;
-	private float _starsMinScale = .5f;
-	private float _starsScaleSpeed = .5f;
-	private float _currentStarsScale = 1f;
+    [Export]
+    private AudioStream _constellationFound1;
 
-	private Vector2 _cameraSpeed = new(200, 200);
+    [Export]
+    private AudioStream _constellationFound2;
 
-	private bool _scanKeyPressed = false;
-	private float _scanTolerance = 100f;
+    private RandomNumberGenerator _rng = new();
 
-	public override void _Ready() {
-		_InitConstellations();
-	}
+    private bool[] _constellationFound;
 
-	public override void _Process(double delta) {
-		_MoveCamera(delta);
-		(int zoomInput, int rotationInput) = _GetCameraInput();
-		_Zoom(zoomInput, delta);
-		_ScanConstellation();
-	}
+    private float _starsMaxScale = 1.5f;
+    private float _starsMinScale = .5f;
+    private float _starsScaleSpeed = .5f;
+    private float _currentStarsScale = 1f;
 
-	private void _InitConstellations() {
-		_constellationFound = new bool[_constellationPositions.Length];
-	}
+    private Vector2 _cameraSpeed = new(200, 200);
 
-	private void _MoveCamera(double delta) {
-		_camera.Position += _GetMovementInput() * _cameraSpeed * (float)delta;
-	}
+    private bool _scanKeyPressed = false;
+    private float _scanTolerance = 100f;
+    private double _scanCooldown = 3;
+    private double _scanCooldownLeft = 0;
 
-	private void _Zoom(int zoomInput, double delta) {
-		if (zoomInput == 0) {
-			return;
-		}
+    public override void _Ready() {
+        _InitConstellations();
+    }
 
-		_currentStarsScale += _starsScaleSpeed * zoomInput * (float)delta;
-		_currentStarsScale = Math.Clamp(_currentStarsScale, _starsMinScale, _starsMaxScale);
-		Vector2 scale = new(_currentStarsScale, _currentStarsScale);
-		foreach (Sprite2D star in _stars) {
-			star.Scale = scale;
-		}
-	}
+    public override void _Process(double delta) {
+        _MoveCamera(delta);
+        (int zoomInput, int rotationInput) = _GetCameraInput();
+        _Zoom(zoomInput, delta);
+        _ReduceCooldown(delta);
+        _ScanConstellation();
+    }
 
-	private void _ScanConstellation() {
-		bool keyPressed = Input.IsKeyPressed(Key.Space);
+    private void _InitConstellations() {
+        _constellationFound = new bool[_constellationPositions.Length];
+    }
 
-		if (!keyPressed) {
-			_scanKeyPressed = false;
-			return;
-		}
+    private void _MoveCamera(double delta) {
+        // TODO: Add out of bounds for the camera
+        _camera.Position += _GetMovementInput() * _cameraSpeed * (float)delta;
+    }
 
-		if (_scanKeyPressed) {
-			return;
-		}
+    private void _Zoom(int zoomInput, double delta) {
+        if (zoomInput == 0) {
+            return;
+        }
 
-		_scanKeyPressed = true;
-		GD.Print($"Scanned at {_camera.Position}");
-		// SCAN
-		// Play SFX for failure or success
-		// Update found constellations
-		for (int i = 0; i < _constellationPositions.Length; i++) {
-			if (_constellationFound[i]) {
-				continue;
-			}
+        _currentStarsScale += _starsScaleSpeed * zoomInput * (float)delta;
+        _currentStarsScale = Math.Clamp(_currentStarsScale, _starsMinScale, _starsMaxScale);
+        Vector2 scale = new(_currentStarsScale, _currentStarsScale);
+        foreach (Sprite2D star in _stars) {
+            star.Scale = scale;
+        }
+    }
 
-			if (!(_camera.Position.DistanceTo(_constellationPositions[i]) <= _scanTolerance)) {
-				continue;
-			}
+    private void _ScanConstellation() {
+        bool keyPressed = Input.IsKeyPressed(Key.Space);
 
-			GD.Print($"Found constellation {i} at {_constellationPositions[i]}");
-			_constellationFound[i] = true;
-			// Play VFX
-			// Play SFX
-			break;
-		}
-	}
+        if (!keyPressed) {
+            _scanKeyPressed = false;
+            return;
+        }
 
-	private Vector2 _GetMovementInput() {
-		Vector2 input = Vector2.Zero;
+        if (_scanKeyPressed) {
+            return;
+        }
 
-		if (Input.IsKeyPressed(Key.W)) {
-			input.Y -= 1;
-		}
+        if (_scanCooldownLeft > 0) {
+            GD.Print($"Scan is on cooldown for {_scanCooldownLeft} seconds");
+            return;
+        }
 
-		if (Input.IsKeyPressed(Key.S)) {
-			input.Y += 1;
-		}
+        _scanCooldownLeft = _scanCooldown;
 
-		if (Input.IsKeyPressed(Key.A)) {
-			input.X -= 1;
-		}
+        // REMOVE TESETING CODE
+        _sfxPlayer.Stream = _rng.RandiRange(0, 1) == 0 ? _constellationFound1 : _constellationFound2;
+        _sfxPlayer.Play();
+        //
 
-		if (Input.IsKeyPressed(Key.D)) {
-			input.X += 1;
-		}
+        _scanKeyPressed = true;
+        GD.Print($"Scanned at {_camera.Position}");
+        for (int i = 0; i < _constellationPositions.Length; i++) {
+            if (_constellationFound[i]) {
+                continue;
+            }
 
-		return input.Normalized();
-	}
+            if (!(_camera.Position.DistanceTo(_constellationPositions[i]) <= _scanTolerance)) {
+                continue;
+            }
 
-	private (int zoomInput, int rotationInput) _GetCameraInput() {
-		int zoomInput = 0;
-		int rotationInput = 0;
+            GD.Print($"Found constellation {i} at {_constellationPositions[i]}");
+            _constellationFound[i] = true;
+            _sfxPlayer.Stream = _rng.RandiRange(0, 1) == 0 ? _constellationFound1 : _constellationFound2;
+            _sfxPlayer.Play();
+            // Play SFX
+            break;
+        }
+    }
 
-		if (Input.IsKeyPressed(Key.Up)) {
-			zoomInput += 1;
-		}
+    private void _ReduceCooldown(double delta) {
+        _scanCooldownLeft -= delta;
+    }
 
-		if (Input.IsKeyPressed(Key.Down)) {
-			zoomInput -= 1;
-		}
+    private Vector2 _GetMovementInput() {
+        Vector2 input = Vector2.Zero;
 
-		if (Input.IsKeyPressed(Key.Left)) {
-			rotationInput -= 1;
-		}
+        if (Input.IsKeyPressed(Key.W)) {
+            input.Y -= 1;
+        }
 
-		if (Input.IsKeyPressed(Key.Right)) {
-			rotationInput += 1;
-		}
+        if (Input.IsKeyPressed(Key.S)) {
+            input.Y += 1;
+        }
 
-		return (
-			zoomInput,
-			rotationInput
-		);
-	}
+        if (Input.IsKeyPressed(Key.A)) {
+            input.X -= 1;
+        }
+
+        if (Input.IsKeyPressed(Key.D)) {
+            input.X += 1;
+        }
+
+        return input.Normalized();
+    }
+
+    private (int zoomInput, int rotationInput) _GetCameraInput() {
+        int zoomInput = 0;
+        int rotationInput = 0;
+
+        if (Input.IsKeyPressed(Key.Up)) {
+            zoomInput += 1;
+        }
+
+        if (Input.IsKeyPressed(Key.Down)) {
+            zoomInput -= 1;
+        }
+
+        if (Input.IsKeyPressed(Key.Left)) {
+            rotationInput -= 1;
+        }
+
+        if (Input.IsKeyPressed(Key.Right)) {
+            rotationInput += 1;
+        }
+
+        return (
+            zoomInput,
+            rotationInput
+        );
+    }
 }
